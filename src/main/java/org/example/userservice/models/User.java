8b1d4f30a3add1e5aad37f51278cb1ba3fff0f2b
sudhanshusingh-g <sudsingh32@gmail.com>
2024-12-27 18:23:08 +0530

    Basic user model and microservice setup

---
+++ src/main/java/org/example/userservice/models/User.java
@@ -0,0 +1,20 @@
+package org.example.userservice.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class User extends BaseModel {
+    private String name;
+    private String password;
+    private String email;
+    @ManyToMany
+    private List<Role> roles;
+}
